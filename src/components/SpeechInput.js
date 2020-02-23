b291a836fc22d1d64546a817b6b8640a42f9a63d
Jon Borgonia <jon.borgonia@gmail.com>
2020-02-22 22:41:46 -0800

    Obtain, store and refresh azure tokens
    - this should be happening on server
    - am fetching from token server directly for demo

--- src/components/SpeechInput.js
+++ src/components/SpeechInput.js
@@ -1,23 +1,23 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { configureSpeechSDK } from '../utils/azure'
 
 const SpeechSDK = window.SpeechSDK
 
-// curl -X POST -d '' -H 'Content-Type: application/json' -H 'Ocp-Apim-Subscription-Key: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' https://westus.api.cognitive.microsoft.com/sts/v1.0/issueToken
-// const tokenResponse = process.env.AZURE_TOKEN
-// FOR DEMO
-const tokenResponse = 'eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJyZWdpb24iOiJ3ZXN0dXMiLCJzdWJzY3JpcHRpb24taWQiOiI5OGZjN2IyOGY1ZWU0YjY2ODM2NDRjNGMwOGI4MTcwNSIsInByb2R1Y3QtaWQiOiJTcGVlY2hTZXJ2aWNlcy5GcmVlIiwiY29nbml0aXZlLXNlcnZpY2VzLWVuZHBvaW50IjoiaHR0cHM6Ly9hcGkuY29nbml0aXZlLm1pY3Jvc29mdC5jb20vaW50ZXJuYWwvdjEuMC8iLCJhenVyZS1yZXNvdXJjZS1pZCI6IiIsInNjb3BlIjoic3BlZWNoc2VydmljZXMiLCJhdWQiOiJ1cm46bXMuc3BlZWNoc2VydmljZXMud2VzdHVzIiwiZXhwIjoxNTgyNDM4MTUwLCJpc3MiOiJ1cm46bXMuY29nbml0aXZlc2VydmljZXMifQ.816AU25z8cb2BcjFnGHffxsM2hhKo47gk6ucdj80h9g'
-const token = JSON.parse(atob(tokenResponse.split(".")[1]));
-const authorizationToken = tokenResponse;
-const speechConfig = SpeechSDK.SpeechConfig.fromAuthorizationToken(authorizationToken, token.region)
-speechConfig.speechRecognitionLanguage = "en-US"
-const audioConfig  = SpeechSDK.AudioConfig.fromDefaultMicrophoneInput()
-
 export default ({setText}) => {
   const [listening, setListening] = useState(false);
+  const [speechConfig, setSpeechConfig] = useState(null);
+  const [audioConfig, setAudioConfig] = useState(null);
 
-  let recognizer = new SpeechSDK.SpeechRecognizer(speechConfig, audioConfig);
+  useEffect(() => {
+    configureSpeechSDK()
+      .then(({speechConfig, audioConfig}) => {
+        setSpeechConfig(speechConfig)
+        setAudioConfig(audioConfig)
+      })
+  }, [])
 
+  let recognizer
   let localBuffer = ''
   const listen = () => {
     localBuffer = ''
--- src/components/SpeechSelection.js
+++ src/components/SpeechSelection.js
@@ -1,17 +1,8 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { configureSpeechSDK } from '../utils/azure'
 
 const SpeechSDK = window.SpeechSDK
-
-// curl -X POST -d '' -H 'Content-Type: application/json' -H 'Ocp-Apim-Subscription-Key: xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx' https://westus.api.cognitive.microsoft.com/sts/v1.0/issueToken
-// const tokenResponse = process.env.AZURE_TOKEN
-// FOR DEMO
-const tokenResponse = 'eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJyZWdpb24iOiJ3ZXN0dXMiLCJzdWJzY3JpcHRpb24taWQiOiI5OGZjN2IyOGY1ZWU0YjY2ODM2NDRjNGMwOGI4MTcwNSIsInByb2R1Y3QtaWQiOiJTcGVlY2hTZXJ2aWNlcy5GcmVlIiwiY29nbml0aXZlLXNlcnZpY2VzLWVuZHBvaW50IjoiaHR0cHM6Ly9hcGkuY29nbml0aXZlLm1pY3Jvc29mdC5jb20vaW50ZXJuYWwvdjEuMC8iLCJhenVyZS1yZXNvdXJjZS1pZCI6IiIsInNjb3BlIjoic3BlZWNoc2VydmljZXMiLCJhdWQiOiJ1cm46bXMuc3BlZWNoc2VydmljZXMud2VzdHVzIiwiZXhwIjoxNTgyNDM2NTAwLCJpc3MiOiJ1cm46bXMuY29nbml0aXZlc2VydmljZXMifQ.VYI3rjTLVQA_4KNHuPhJz9fA4WaDGNbXyQs1R5z8zeY'
-const token = JSON.parse(atob(tokenResponse.split(".")[1]));
-const authorizationToken = tokenResponse;
-const speechConfig = SpeechSDK.SpeechConfig.fromAuthorizationToken(authorizationToken, token.region)
-speechConfig.speechRecognitionLanguage = "en-US"
-const audioConfig  = SpeechSDK.AudioConfig.fromDefaultMicrophoneInput()
 
 const selectChoice = (selections, speechText) => {
   const choice = selections.find(({ matches }) =>
@@ -48,8 +39,18 @@ const selectChoice = (selections, speechText) => {
  */
 export default ({selections, navigation}) => {
   const [debug, setDebug] = useState('');
-  let recognizer
+  const [speechConfig, setSpeechConfig] = useState(null);
+  const [audioConfig, setAudioConfig] = useState(null);
 
+  useEffect(() => {
+    configureSpeechSDK()
+      .then(({speechConfig, audioConfig}) => {
+        setSpeechConfig(speechConfig)
+        setAudioConfig(audioConfig)
+      })
+  }, [])
+
+  let recognizer
   let localBuffer = ''
   const listen = () => {
     localBuffer = ''
