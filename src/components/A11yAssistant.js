2affa042c8414647ee8bcc4d124f92fb79187ed2
Jon Borgonia <jon.borgonia@gmail.com>
2020-02-24 20:17:54 -0800

    a11y speech assistant constantly listens until toggled

--- src/components/A11yAssistant.js
+++ src/components/A11yAssistant.js
@@ -25,6 +25,24 @@ const selectChoice = (selections, navigation, speechText) => {
   return []
 }
 
+// @TODO need localBuffer for long text area
+const speechParser = (localBuffer, setLocalBuffer, setA11yOutput, selections, navigation) => (sender, recognition) => {
+  window.console.log(recognition.result.text);
+  setA11yOutput(recognition.result.text) // don't destructure result
+
+  const choices = selectChoice(selections, navigation, recognition.result.text)
+  window.console.log(recognition.result.text, 'chose selections:', choices.map(c => c.name));
+
+  if(choices){
+    setA11yOutput(recognition.result.text + '\nchose selections: ' + choices.map(c => c.name))
+    // reset localBuffer
+    // setLocalBuffer('')
+  } else {
+    // setLocalBuffer(recognition.result.text)
+  }
+
+}
+
 /*
  * selections : [
  *   {
@@ -57,59 +75,60 @@ const selectChoice = (selections, navigation, speechText) => {
  */
 export default () => {
   const [{ a11yAssistant }] = useStateValue();
-  const [allyOutput, setAllyOutput] = useState('');
+  const [localBuffer, setLocalBuffer] = useState('') // what we use to parse
+  const [a11yOutput, setA11yOutput] = useState('') // what is displayed to user
   const [listening, setListening] = useState(false);
-  const [speechConfig, setSpeechConfig] = useState(null);
-  const [audioConfig, setAudioConfig] = useState(null);
+  const [recognizer, setRecognizer] = useState(null);
 
-  // const { selections, navigation } = a11yAssistant
-  const selections = a11yAssistant.selections
-  const navigation = a11yAssistant.navigation
+  const { selections, navigation } = a11yAssistant
 
   useEffect(() => {
     configureSpeechSDK()
       .then(({speechConfig, audioConfig}) => {
-        setSpeechConfig(speechConfig)
-        setAudioConfig(audioConfig)
+        const recognizer = new SpeechSDK.SpeechRecognizer(speechConfig, audioConfig)
+        recognizer.recognizing = speechParser(localBuffer, setLocalBuffer, setA11yOutput, selections, navigation)
+        setRecognizer(recognizer)
       })
-  }, [])
+  }, [localBuffer, selections, navigation])
 
-  let recognizer
-  let localBuffer = ''
-  const listen = () => {
-    setListening(true)
-    localBuffer = ''
-    setAllyOutput('Listening...')
-    recognizer = new SpeechSDK.SpeechRecognizer(speechConfig, audioConfig)
-    recognizer.recognizeOnceAsync(
-      result => {
-        localBuffer += result.text
 
-        const choices = selectChoice(selections, navigation, localBuffer)
-        window.console.log(result, 'chose selections:', choices.map(c => c.name));
+  const toggleListen = () => {
+    if(!listening){
+      setListening(true)
+      recognizer.startContinuousRecognitionAsync(
+        () => {
+          setA11yOutput('Listening...')
+        },
+        err => {
+          setA11yOutput(err.toString())
+          window.console.log(err);
 
-        if(choices){
-          setAllyOutput(localBuffer + '\nchose selections: ' + choices.map(c => c.name))
-        }
+          recognizer.close();
+          setListening(false)
+        })
+    } else {
+      setListening(false)
+      recognizer.stopContinuousRecognitionAsync(
+        () => {
+          setA11yOutput('Stopped Listening...')
+        },
+        err => {
+          setA11yOutput(err.toString())
+          window.console.log(err);
 
-        recognizer.close();
-        setListening(false)
-      },
-      err => {
-        setAllyOutput(err.toString())
-        window.console.log(err);
+          recognizer.close();
+          setListening(false)
+        })
+    }
 
-        recognizer.close();
-        setListening(false)
-      });
   }
 
   return (
     <div className={`air-ally-assistant-container air-ally-assistant-speechselection-container ${listening ? 'air-ally-assistant-container-listening':''}`}>
-      <div className={`speech-button ${listening ? 'speech-button-listening':''}`} onClick={listen}>
+      <div className={`speech-button ${listening ? 'speech-button-listening':''}`} onClick={toggleListen}>
         <span className="speech-button-text"><i className="fas fa-microphone"></i> Air Ally Assistant</span>
       </div>
-      <div className="air-ally-assistant-output"><span>{allyOutput}</span></div>
+      <div className="air-ally-assistant-output"><span>{a11yOutput}</span></div>
     </div>
   )
 }
